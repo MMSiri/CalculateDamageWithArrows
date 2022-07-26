01acc127dfe1f095ce6c10d329bee4507c43de27
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-07-26 20:31:07 +0100

    Add project files.

---
+++ CalculateDamageWithArrows/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CalculateDamageWithArrows
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                SwordDamage swordDamage = new SwordDamage(RollDice(3));
+                ArrowDamage arrowDamage = new ArrowDamage(RollDice(1));
+
+                Console.WriteLine("Enter '0' for no Magic/Flaming, '1' for Magic, '2' for Flaming, '3' for Both, any other key to exit:");
+                char input = Console.ReadKey(false).KeyChar;
+                if (input != '0' && input != '1' && input != '2' && input != '3') return;
+
+
+                Console.Write("\n'S' for Sword, 'A' for Arrow, any other key to exit: ");
+                char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
+
+                switch (weaponKey)
+                {
+                    case 'S':
+                        swordDamage.Roll = RollDice(3);
+                        swordDamage.Magic = (input == '1' || input == '3');
+                        swordDamage.Flaming = (input == '2' || input == '3');
+                        Console.WriteLine('\n' + $"Rolled {swordDamage.Roll} for {swordDamage.Damage}HP of damage");
+                        break;
+
+                    case 'A':
+                        arrowDamage.Roll = RollDice(1);
+                        arrowDamage.Magic = (input == '1' || input == '3');
+                        arrowDamage.Flaming = (input == '2' || input == '3');
+                        Console.WriteLine('\n' + $"Rolled {arrowDamage.Roll} for {arrowDamage.Damage}HP of damage");
+                        break;
+
+                    default:
+                        return;
+                }
+
+            }
+        }
+
+        public static int RollDice(int numberOfRolls)
+        {
+            int diceTotal = 0;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                diceTotal += random.Next(1, 7);
+            }
+            return diceTotal;
+        }
+        }
+    }
+}
---
+++ CalculateDamageWithArrows/SwordDamage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateDamageWithArrows
+{
+    class SwordDamage
+    {
+        /// <summary>
+        /// Constructor calcs damage based on default Magic and Flaming values and a starting 3d6 roll.
+        /// </summary>
+        /// <param name="startingRoll">starting 3d6 roll</param>
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+
+
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        private int roll;
+
+        /// <summary>
+        /// Sets or gets the 3d6 roll.
+        /// </summary>
+        public int Roll { get { return roll; } set { roll = value; CalculateDamage(); } }
+
+
+        private bool flaming;
+        /// <summary>
+        /// True if sword is flaming. False otherwise.
+        /// </summary>
+        public bool Flaming { get { return flaming; } set { flaming = value; CalculateDamage(); } }
+
+        private bool magic;
+
+        /// <summary>
+        /// True if sword is magic. False otherwise.
+        /// </summary>
+        public bool Magic { get { return magic; } set { magic = value; CalculateDamage(); } }
+
+        /// <summary>
+        /// Contains the calculated damage.
+        /// </summary>
+        public int Damage { get; private set; }
+
+        /// <summary>
+        /// Calcs damage based on current properties.
+        /// </summary>
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+    }
+}
