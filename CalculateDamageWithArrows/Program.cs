bae268f6096f0daca3ea4f2f9e75ca02df44751c
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-07-27 17:52:48 +0100

    Improved encapsulation and refactored code

--- CalculateDamageWithArrows/Program.cs
+++ CalculateDamageWithArrows/Program.cs
@@ -6,13 +6,10 @@ namespace CalculateDamageWithArrows
     {
         static Random random = new Random();
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             while (true)
             {
-                SwordDamage swordDamage = new SwordDamage(RollDice(3));
-                ArrowDamage arrowDamage = new ArrowDamage(RollDice(1));
-
                 Console.WriteLine("Enter '0' for no Magic/Flaming, '1' for Magic, '2' for Flaming, '3' for Both, any other key to exit:");
                 char input = Console.ReadKey(false).KeyChar;
                 if (input != '0' && input != '1' && input != '2' && input != '3') return;
@@ -24,17 +21,11 @@ namespace CalculateDamageWithArrows
                 switch (weaponKey)
                 {
                     case 'S':
-                        swordDamage.Roll = RollDice(3);
-                        swordDamage.Magic = (input == '1' || input == '3');
-                        swordDamage.Flaming = (input == '2' || input == '3');
-                        Console.WriteLine('\n' + $"Rolled {swordDamage.Roll} for {swordDamage.Damage}HP of damage");
+                        WeaponDamageOutput(input, weaponKey);
                         break;
 
                     case 'A':
-                        arrowDamage.Roll = RollDice(1);
-                        arrowDamage.Magic = (input == '1' || input == '3');
-                        arrowDamage.Flaming = (input == '2' || input == '3');
-                        Console.WriteLine('\n' + $"Rolled {arrowDamage.Roll} for {arrowDamage.Damage}HP of damage");
+                        WeaponDamageOutput(input, weaponKey);
                         break;
 
                     default:
@@ -44,7 +35,7 @@ namespace CalculateDamageWithArrows
             }
         }
 
-        public static int RollDice(int numberOfRolls)
+        private static int RollDice(int numberOfRolls)
         {
             int diceTotal = 0;
             for (int i = 0; i < numberOfRolls; i++)
@@ -53,6 +44,27 @@ namespace CalculateDamageWithArrows
             }
             return diceTotal;
         }
+
+        private static void WeaponDamageOutput(char damageKey, char weaponKey)
+        {
+            SwordDamage swordDamage = new SwordDamage(RollDice(3));
+            ArrowDamage arrowDamage = new ArrowDamage(RollDice(1));
+
+            if (weaponKey == 'S') 
+            { 
+                swordDamage.Roll = RollDice(3);
+                swordDamage.Magic = (damageKey == '1' || damageKey == '3');
+                swordDamage.Flaming = (damageKey == '2' || damageKey == '3');
+                Console.WriteLine('\n' + $"Rolled {swordDamage.Roll} for {swordDamage.Damage}HP of damage");
+            }
+
+            if (weaponKey == 'A')
+            {
+                arrowDamage.Roll = RollDice(1);
+                arrowDamage.Magic = (damageKey == '1' || damageKey == '3');
+                arrowDamage.Flaming = (damageKey == '2' || damageKey == '3');
+                Console.WriteLine('\n' + $"Rolled {arrowDamage.Roll} for {arrowDamage.Damage}HP of damage");
+            }
+        }
         }
     }
-}
